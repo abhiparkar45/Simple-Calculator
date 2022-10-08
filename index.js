5fb63567a6dfe44a6e1f7eafaffa39dc08d9af17
abhiparkar45 <abhishekparkar48@gmail.com>
2022-10-08 15:12:24 +0530

    v3.0.0

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const calculate = (operation) => {
-  let val1 = Number(document.getElementById("firstValue").value);
-  let val2 = Number(document.getElementById("secondValue").value);
-  let val3;
+  let val1 = document.getElementById("firstValue").value;
+  let val2 = document.getElementById("secondValue").value;
+
+  if(!val1 || !val2 || isNaN(val1) || isNaN(val2)){
+    (!val1 || !val2)? (alert("Please Enter both values !")):(alert("Invalid values !"))
+    return;
+  }
+  let val3 = Number(val1);
+  let val4 = Number(val2);
+  let res;
 
   switch (operation) {
     case "+":
-      val3 = val1 + val2;
-      document.getElementById("result").value = val3;
+      res = val3 + val4;
+      document.getElementById("result").value = res;
       break;
     case "-":
-      val3 = val1 - val2;
-      document.getElementById("result").value = val3;
+      res = val3 - val4;
+      document.getElementById("result").value = res;
       break;
     case "*":
-      val3 = val1 * val2;
-      document.getElementById("result").value = val3;
+      res = val3 * val4;
+      document.getElementById("result").value = res;
       break;
     case "/":
-      val3 = val1 / val2;
-      document.getElementById("result").value = val3;
+      res = val3 / val4;
+      document.getElementById("result").value = res;
       break;
   }
 };
